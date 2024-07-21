1377ea7dff14cc24ca48b98eafe9ccba68d1b94b
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-07-21 18:52:33 +0100

    admin data

--- frontend/src/app/home-admin/home-admin.component.ts
+++ frontend/src/app/home-admin/home-admin.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';  // Add this import
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+
 @Component({
   selector: 'app-home-admin',
   standalone: true,
-  imports: [FormsModule, CommonModule ],
+  imports: [FormsModule, CommonModule, HttpClientModule ],
   templateUrl: './home-admin.component.html',
-  styleUrl: './home-admin.component.scss'
+  styleUrl: './home-admin.component.scss',
+  providers: [AuthService]
 })
-export class HomeAdminComponent {
+export class HomeAdminComponent implements OnInit {
+  constructor(private authService: AuthService){}
   azureCreds = {
     tenantId: 'your-tenant-id',
     clientId: 'your-client-id',
@@ -22,6 +27,11 @@ export class HomeAdminComponent {
     clientSecret: false,
     subscriptionId: false
   };
+  firstname: string = "";
+  lastname: string = "";
+  email: string = "";
+  id: any;
+  matricule: any;
 
   editCredential(key: string) {
     this.editing[key] = true;
@@ -37,5 +47,21 @@ export class HomeAdminComponent {
     }).catch(err => {
       console.error('Failed to copy text: ', err);
     });
+  }
+
+  ngOnInit() {
+    // Check if token exists after component initialization
+    const token = this.authService.getToken();
+    if (token) {
+      // Extract user data (consider using a secure backend API instead)
+      const decodedPayload = atob(token.split('.')[1]);
+      const userData = JSON.parse(decodedPayload);
+      console.log(userData)
+      this.firstname = userData.firstname;
+      this.lastname = userData.lastname;
+      this.matricule = userData.matricule;
+      this.email = userData.email;
+      this.id = userData.id
+    }
   }
 }
